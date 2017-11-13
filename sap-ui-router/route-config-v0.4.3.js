4ca3763e30ed49254eac47355f69d39c55ad0c2e
NEXT_IT\Wakidur <wakidur@gmail.com>
2017-11-13 17:09:42 +0600

    #sample app add

--- sap-ui-router/route-config-v0.4.3.js
+++ sap-ui-router/route-config-v0.4.3.js
@@ -5,12 +5,21 @@
             .module('appModule')
             .config(configConfig);
 
-        configConfig.$inject = ['$stateProvider'];
+        configConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
 
-        function configConfig($stateProvider) {
+        function configConfig($stateProvider , $urlRouterProvider) {
+            $stateProvider.state('root', {
+                url: '/',
+                template: '<h1>Home page </h1>'
+            });
             $stateProvider.state('contacts', {
-                url: '/contacts',
-                template: '<h1>My Contacts</h1>'
+                // url: '/contacts/:a/:b',
+                url: '/contacts/{a}/{b}',
+                template: '<h1>My Contacts = {{a}} || {{b}}</h1>',
+                controller: function($scope, $stateParams){
+                    $scope.a = $stateParams.a;
+                    $scope.b = $stateParams.b;
+                }
             });
             $stateProvider.state('settings', {
                 url: '/settings',
@@ -20,10 +29,12 @@
                 url: '/logout',
                 templateUrl: 'views/logout.html'
             });
-            $stateProvider.state('nofound', {
-                url: '*path',
-                template: '<h2> No route found  </h2>'
-            });
+            // $stateProvider.state('nofound', {
+            //     url: '*path',
+            //     template: '<h2> No route found  </h2>'
+            // });
+            // Super Special
+            $urlRouterProvider.otherwise('/');
 
         }
 
