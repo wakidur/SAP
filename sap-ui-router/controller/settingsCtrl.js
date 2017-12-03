cd54c37377e2e3daf2d5f3d5a1b70a89c448d5b8
NEXT_IT\Wakidur <wakidur@gmail.com>
2017-12-03 10:03:07 +0600

    #how to split the ng-repeat data with three columns using bootstrap

--- sap-ui-router/controller/settingsCtrl.js
+++ sap-ui-router/controller/settingsCtrl.js
@@ -7,6 +7,107 @@
     constructor.$inject = ['$scope', '$stateParams', '$log'];
 
     function constructor($scope, $stateParams, $log) {
+        var myObject = [{
+                name: "Wakidur",
+                itme: 120
+            },
+            {
+                name: "most",
+                itme: 2
+            },
+            {
+                name: "reliable",
+                itme: 12
+            },
+            {
+                name: "technically ",
+                itme: 172
+            },
+            {
+                name: "correct ",
+                itme: 0
+            },
+            {
+                name: "approach ",
+                itme: 1
+            }
+        ];
+
+        function chunk(arr, size) {
+            var newArr = [];
+            for (var i=0; i<arr.length; i+=((arr.length)/size)) {
+              newArr.push(arr.slice(i, i+((arr.length)/size)));
+            }
+            return newArr;
+          }
+         
+        $scope.arrayOfObjectValude = [
+            {
+                name: "Wakidur",
+                itme: 120
+            },
+            {
+                name: "most",
+                itme: 2
+            },
+            {
+                name: "reliable",
+                itme: 12
+            },
+            {
+                name: "technically ",
+                itme: 172
+            },
+            {
+                name: "correct ",
+                itme: 5
+            },
+            {
+                name: "correct ",
+                itme: 9
+            },
+            {
+                name: "correct ",
+                itme: 78
+            },
+            {
+                name: "correct ",
+                itme: 0
+            },
+            {
+                name: "correct ",
+                itme: 98
+            },
+            {
+                name: "correct ",
+                itme: 69
+            },
+            {
+                name: "correct ",
+                itme: 69
+            },
+            {
+                name: "correct ",
+                itme:78
+            },
+            {
+                name: "correct ",
+                itme: 45
+            },
+            {
+                name: "correct ",
+                itme: 36
+            },
+            {
+                name: "correct ",
+                itme: 33333
+            },
+            {
+                name: "approach ",
+                itme: 1
+            }
+        ];
+        $scope.arrayOfObject = chunk($scope.arrayOfObjectValude, 3);
 
 
         // $scope.grids = ['horizontal','Stacked'];
@@ -48,5 +149,3 @@
     }
 
 })(window.angular);
-
-
