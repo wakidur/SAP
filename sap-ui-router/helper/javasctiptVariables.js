c8513f5200e42c04c1d430eb1af4ac9b421fcc9c
NEXT_IT\Wakidur <wakidur@gmail.com>
2017-12-14 11:27:46 +0600

    #ui change

--- sap-ui-router/helper/javasctiptVariables.js
+++ sap-ui-router/helper/javasctiptVariables.js
@@ -1,16 +1,4 @@
 //app is being used for application module. all business module will be loaded into app dynamically.
-var ERP_MODULES = {
-    APP: { name: 'app', code: 0 },
-    ADMIN: { name: 'ADMIN', code: 1 },
-    HRM: { name: 'HR', code: 2 },
-    GS: { name: 'GS', code: 3 },
-    OM: { name: 'OM', code: 4 },
-    COM: { name: 'COM', code: 5 },
-    PM: { name: 'PM', code: 6 },
-    FA: { name: 'FA', code: 7 },
-    PD: { name: 'PD', code: 8 },
-    MGP: { name: 'MGP', code: 9 },
-};
 //when someone click menu, then clicked menu information is stored into pageInfo
 //for using whole application.
 var pageInfo = {
