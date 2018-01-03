e5cf8a4623c188747de8c5f62423065346db7b1b
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-01-03 14:29:40 +0600

    #stack over flow problem solve

--- stackoverflow.com/problemAndSolution/Jan-18/example-1-helper-2.js
+++ stackoverflow.com/problemAndSolution/Jan-18/example-1-helper-2.js
@@ -1,3 +1,4 @@
+// https://www.codeproject.com/Articles/1041128/Dynamically-Creating-and-Showing-Bootstrap-Modal
 var BstrapModal = function (title, body, buttons) {
     var title = title || "Lorem Ipsum History";
     var body = body || "Lorem Ipsum is simply dummy text of the printing and typesetting industry.  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,  when an unknown printer took a galley of type and scrambled it to make a type specimen book.  It has survived not only five centuries, but also the leap into electronic typesetting,  remaining essentially unchanged.";
