017e0e90579913f8df4b298b6c3196df77866f27
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-01-04 12:16:40 +0600

    #add bootstrap 3.3.7 version

--- stackoverflow.com/problemAndSolution/Jan-18/example-1-helper-2.js
+++ stackoverflow.com/problemAndSolution/Jan-18/example-1-helper-2.js
@@ -17,11 +17,13 @@ var BstrapModal = function (title, body, buttons) {
             buttonshtml += "<button type='button' class='btn " + (buttons[i].Css || "") + "' name='btn" + that.Id + "'>" + (buttons[i].Value || "CLOSE") + "</button>";
         }
         return "<div class='modal fade' name='dynamiccustommodal' id='" + that.Id + "' tabindex='-1' role='dialog'   data-backdrop='static' data-keyboard='false' aria-labelledby='" + that.Id + "Label'><div class='modal-dialog'> <div class='modal-content'><div class='modal-header'> <button type='button' class='close modal-white-close'   onclick='BstrapModal.Close()'><span aria-hidden='true'>&times; </span></button><h4 class='modal-title'>" + title +
-            "</h4></div><div class='modal-body'> <div class='row'><div class='col-xs-12 col-md-12 col-sm-12 col-lg-12'>" + body + "</div></div></div><div class='modal-footer bg-default'> <div class='col-xs-12 col-sm-12 col-lg-12'>" + buttonshtml + "</div></div></div></div></div>";
+            "</h4></div><div class='modal-body'> <div class='row'><div class='col-xs-12 col-sm-12 col-md-12  col-lg-12'>" + body + "</div></div></div><div class='modal-footer bg-default'> <div class='col-xs-12 col-sm-12 col-md-12 col-lg-12'>" + buttonshtml + "</div></div></div></div></div>";
     }();
     BstrapModal.Delete = function () {
         var modals = document.getElementsByName("dynamiccustommodal");
-        if (modals.length > 0) document.body.removeChild(modals[0]);
+        if (modals.length > 0) {
+            document.body.removeChild(modals[0]);
+        }
     };
     BstrapModal.Close = function () {
         $(document.getElementById(BstrapModal.Id)).modal('hide');
