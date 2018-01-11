288cd731726cade1482a2d9c68e6eb0077e5d88a
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-01-11 18:34:38 +0600

    #add style

--- admin-app/appConfig.js
+++ admin-app/appConfig.js
@@ -12,3 +12,22 @@
     }
 
 }());
+
+(function(){
+    'use strict';
+
+    angular
+        .module('adminApp')
+        .run(runRun);
+
+    runRun.$inject = ['$rootScope', '$state', '$stateParams'];
+
+    function runRun($rootScope, $state, $stateParams){
+        $rootScope.$on('$stateChangeSuccess', function () {
+            document.body.scrollTop = document.documentElement.scrollTop = 0;
+          });
+          $rootScope.$state = $state;
+          return $rootScope.$stateParams = $stateParams;
+    }
+
+}());
