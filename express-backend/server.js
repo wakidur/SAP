f2d13126d7851951d500ec79bc9439ffdf154ac5
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-01-16 10:23:02 +0600

    #start chapter 1 in javscript books

--- express-backend/server.js
+++ express-backend/server.js
@@ -2,9 +2,26 @@
 const express = require('express');
 
 const app = express();
-app.get('/',  (req, res) => {
-    res.send('Hello World!');
+// make '/app' default route
+app.get('/', function (req, res) {
+  var options = {
+    root: __dirname + '/client/',
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+
+  var fileName = req.params.name;
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      next(err);
+    } else {
+      console.log('Sent:', fileName);
+    }
   });
+});
 let server =  app.listen( 3000, () => {
     console.log("Example listening on port 3000");
     let host = server.address().address;
