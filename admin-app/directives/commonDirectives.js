c47b371dafddab7ed2485a96574ac81d2861a66a
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-01-25 17:14:19 +0600

    #new file add

--- admin-app/directives/commonDirectives.js
+++ admin-app/directives/commonDirectives.js
@@ -10,7 +10,79 @@ angular
 .directive('div', bootstrapCarouselDirective)
 .directive('toggle', bootstrapTooltipsPopoversDirective)
 .directive('tab', bootstrapTabsDirective)
-.directive('button', cardCollapseDirective);
+.directive('button', cardCollapseDirective)
+.directive('sideNavigation', sideNavigation)
+.directive('minimalizaSidebar', minimalizaSidebar)
+/**
+ * sideNavigation - Directive for run metsiMenu on sidebar navigation
+ */
+function sideNavigation($timeout) {
+  return {
+      restrict: 'A',
+      link: function(scope, element) {
+          // Call the metsiMenu plugin and plug it to sidebar navigation
+          // $timeout(function(){
+          //     element.metisMenu();
+
+          // });
+
+          // Colapse menu in mobile mode after click on element
+          var menuElement = $('#side-menu a:not([href$="\\#"])');
+          menuElement.click(function(){
+              if ($(window).width() < 769) {
+                  $("body").toggleClass("mini-navbar");
+              }
+          });
+
+          // Enable initial fixed sidebar
+          if ($("body").hasClass('fixed-sidebar')) {
+              var sidebar = element.parent();
+              sidebar.slimScroll({
+                  height: '100%',
+                  railOpacity: 0.9,
+              });
+          }
+      }
+  };
+};
+
+/**
+ * minimalizaSidebar - Directive for minimalize sidebar
+*/
+function minimalizaSidebar($timeout) {
+  return {
+      restrict: 'A',
+      template: '<a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="" ng-click="minimalize()"><i class="fa fa-bars"></i></a>',
+      controller: function ($scope, $element) {
+          $scope.minimalize = function () {
+              $("body").toggleClass("mini-navbar");
+              if (!$('body').hasClass('mini-navbar') || $('body').hasClass('body-small')) {
+                  // Hide menu in order to smoothly turn on when maximize menu
+                  $('#side-menu').hide();
+                  // For smoothly turn on menu
+                  setTimeout(
+                      function () {
+                          $('#side-menu').fadeIn(400);
+                      }, 200);
+              } else if ($('body').hasClass('fixed-sidebar')){
+                  $('#side-menu').hide();
+                  setTimeout(
+                      function () {
+                          $('#side-menu').fadeIn(400);
+                      }, 100);
+              } else {
+                  // Remove all inline style from jquery fadeIn function to reset menu state
+                  $('#side-menu').removeAttr('style');
+              }
+          }
+      }
+  };
+};
+
+
+
+
+
 
 //Prevent click if href="#"
 function preventClickDirective() {
