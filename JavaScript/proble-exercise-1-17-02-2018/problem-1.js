6bd6803207f5eae4a24cb9023c6453115010d924
wakidur <wakidr@gmail.com>
2018-02-17 21:15:32 +0600

    #add exmaple

--- JavaScript/proble-exercise-1-17-02-2018/problem-1.js
+++ JavaScript/proble-exercise-1-17-02-2018/problem-1.js
@@ -1,15 +1,31 @@
 function deletBookmark(name) {
-	var bookmarks = json.parse(localStorage.getItem('bookmarks'));
-	// bookmarks.splice(0,1);
-	for( var i = 0; i < bookmarks.length ; i++ ) {
-		// var x = bookmarks[i].name;
-		// var y = name;
-		if(bookmarks[i].name == name) {
-			bookmarks.splice(i, 1);
-		}
-	}
-	localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-	fatchBookmarks();
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    // bookmarks.splice(0,1);
+    for (var i = 0; i < bookmarks.length; i++) {
+        // var x = bookmarks[i].name;
+        // var y = name;
+        if (bookmarks[i].name == name) {
+            bookmarks.splice(i, 1);
+        }
+    }
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    fatchBookmarks();
 }
 
-function fatchBookmarks(){}
+function fatchBookmarks() {
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    var bookmarksResults = document.getElementById('bookmarksResults');
+
+    bookmarksResults.innerHTML = "";
+    for (var i = 0; i < bookmarks.length; i++) {
+        var name = bookmarks[i].name;
+        var url = bookmarks[i].url;
+
+        bookmarksResults.innerHTML += "<div class='bdr'>" +
+            "<h3>" + name +
+            "<a class='btn btn-primary' target='_blank' href='" + url + "'> Visit</a> " +
+            "<a class='btn btn-primary' onclick='deletBookmark(\" " + name + "\")'> Delete</a> " +
+            "</h3>" +
+            "</div>";
+    }
+}
