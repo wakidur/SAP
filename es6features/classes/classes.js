cb12ce22290515d6e2e170bf5b0a983832d64519
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-03-05 12:17:30 +0600

    #class practice

--- es6features/classes/classes.js
+++ es6features/classes/classes.js
@@ -6,7 +6,7 @@
 
 //  Example function expressions and declarations
 // function expression
-var notHoisted = function() {
+var notHoisted = function () {
     console.log('bar');
 };
 //  Function declaration
@@ -57,10 +57,10 @@ var Rectangle = class Rectangle {
 
 class Rectangle {
     constructor(height, width) {
-            this.height = height;
-            this.width = width;
-        }
-        // Getter
+        this.height = height;
+        this.width = width;
+    }
+    // Getter
     get area() {
         return this.calcArea();
     }
@@ -96,3 +96,106 @@ const p1 = new point(5, 5);
 const p2 = new point(10, 10);
 
 console.log(point.distance(p1, p2));
+
+
+//  Boxing with prototype and static methods
+
+class Animal {
+    speak() {
+        return this;
+    }
+
+    static eat() {
+        return this;
+    }
+}
+
+let obj = new Animal();
+obj.speak(); // Animal {}
+let speak = obj.speak;
+speak(); // undefined
+
+Animal.eat() // call Animal
+
+let eat = Animal.eat;
+
+eat(); // undefined
+
+
+// Traditional function base classes
+
+function Animals() {}
+
+Animals.prototype.speak = function () {
+    return this;
+}
+
+Animals.eat = function() {
+    return this;
+}
+
+let  objT = new Animals();
+let sepek = objT.speak;
+speak();
+
+let  eata = Animals.eat;
+eata();
+
+// Sub classing with extends
+
+class AnimalParentCla{
+    constructor(name) {
+        this.name = name;
+    }
+
+    speak() {
+        console.log(this.name + ' makes a noise.');
+    }
+}
+
+class Dog extends AnimalParentCla {
+    speak() {
+        console.log(this.name + " barks.");
+    }
+}
+
+const d = new Dog('Mitzie');
+
+d.speak();
+
+
+// extend traditional function-based
+
+function AnimalParent(name) {
+    this.name = name;
+}
+
+AnimalParent.prototype.speak = function () {
+    console.log(this.name + ' makes a noise.');
+}
+
+class Dogs extends AnimalParent {
+    speak() {
+        console.log(this.name + ' barks.');
+    }
+} 
+const dogss = new Dogs('Mitzie');
+dogss.speak();// mitzie barks.
+
+
+// want to inherit from a regular object
+var myAnimal = {
+    speak() {
+        console.log( this.name + ' makes a noise.');
+    }
+};
+
+class myDob {
+    constructor(name) {
+        this.name = name;
+    }
+};
+
+
+// if you do not do this you will get a TypeError when you invoke speak
+Object.setPrototypeOf(myDob.prototype , myAnimal);
