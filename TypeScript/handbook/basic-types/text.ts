fe21e866a9968f488f86142cc873fc5b61643727
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-03-27 15:48:20 +0600

    #add componet archituchet

--- TypeScript/handbook/basic-types/text.ts
+++ TypeScript/handbook/basic-types/text.ts
@@ -1,1 +1,41 @@
-7a
+// 27-03-2018
+/**
+ * Boolean
+*/
+
+let isDone: boolean = false;
+let isDoneTrue: boolean = true;
+
+
+/**
+ * Number
+ */
+
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+
+
+/**
+ * String
+ */
+
+ let color: string = "blue";
+    color = 'red';
+
+    // Template strings
+
+let fullName: string = `Bob Bobbington`;
+let age:number = 32;
+let sentence: string = `Hello, my name is ${ fullName }. I'will be ${ age + 1 } years old next month.`;
+
+// this is equivalent to declaring sentence like.
+
+let sentenceEquivalent = "Hello, my name is " + fullName + ".\n\n" + "I'll be " + (age + 1) + "Years old next month.";
+
+/**
+ * Array
+ */
+
+
