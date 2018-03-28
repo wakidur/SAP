e2e12908c08961c1bf3ef446bae55fe1537e52b4
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-03-28 17:35:31 +0600

    #aa

---
+++ stackoverflow.com/problemAndSolution/Mar-2018/angularjs/discoverCtrl.js
@@ -0,0 +1,16 @@
+(function(){
+    'use strict';
+
+    angular
+        .module('module')
+        .controller('discoverCtrl', construnctor)
+
+        construnctor.$inject = ['$scop', '$q'];
+
+    function construnctor($scope, $q) {
+        /* jshint validthis:true */
+        var vm = this;
+
+        
+    }
+})();
---
+++ stackoverflow.com/problemAndSolution/Mar-2018/angularjs/gethostsbyipService.js
@@ -0,0 +1,28 @@
+// https://stackoverflow.com/questions/49529400/angularjs-promises-controller-service-doesnt-work
+(function(){
+    'use strict';
+
+    angular
+        .module('module')
+        .service('gethostsbyip', constructor)
+
+        constructor.$inject = ['$http', '$q'];
+
+    function constructor($http, $q) {
+        this.gethostsbyip  = gethostsbyip ;
+
+        function gethostsbyip (hostname, username, password) {
+            var deffered = $q.defer();
+            $http.post( REST_API_IP + 'discover?ip=' + hostname  + '&username=' + username  + '&password' + password).then(function (resp) { 
+                deffered.resolve(resp);
+                location.href="#createvirtualization";
+                toastr.success('Your hosts, templates, networks have been updated!', 'Data was loaded!');
+             }).catch(function(e){
+                toastr.error('Some data in your form is incorrect. Please, try again!', 'Error!');
+            });
+            return deffered.promise;
+
+         }
+        
+    }
+})();
