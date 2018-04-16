6204c4ed3d0f634dd15bd57e1df1d92b5a81556d
wakidur <wakidr@gmail.com>
2018-04-16 20:25:00 +0600

    tere

--- demo/node-rest-api-jwt-security-application-master/config/db.js
+++ demo/node-rest-api-jwt-security-application-master/config/db.js
@@ -1,2 +1,18 @@
+/*
 let mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost/node-jwt-db");
+
+*/
+//Import the mongoose module
+let mongoose = require('mongoose');
+
+//Set up default mongoose connection
+let mongoDB = 'mongodb://localhost/node-jwt-db';
+mongoose.connect(mongoDB);
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+//Get the default connection
+let db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
