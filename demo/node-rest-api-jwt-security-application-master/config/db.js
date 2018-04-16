0832de40b2e6630b600452ff5c02a4a698e22e44
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-04-16 12:58:19 +0600

    #add

--- demo/node-rest-api-jwt-security-application-master/config/db.js
+++ demo/node-rest-api-jwt-security-application-master/config/db.js
@@ -1,2 +1,2 @@
 let mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/node-jwt-db');
+mongoose.connect('mongodb://localhost/node-jwt-db');
--- demo/node-rest-api-jwt-security-application-master/models/User.js
+++ demo/node-rest-api-jwt-security-application-master/models/User.js
@@ -7,15 +7,3 @@ let UserSchema = new mongoose.Schema({
 mongoose.model('User', UserSchema);
 
 module.exports = mongoose.model('User');
-
-/*
-let mongoose = require('mongoose');
-let Schema = mongoose.Schema;
-let userSchema = new Schema({
-	name: String,
-	email: String,
-	password: String
-});
-
-module.exports = mongoose.model('User', userSchema);
-*/
