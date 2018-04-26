72ea118ad34072ecd38b77c2ed205dea37489656
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-04-26 16:45:34 +0600

    #add new file

--- TypeScript/handbook/variable-declarations/declaration.ts
+++ TypeScript/handbook/variable-declarations/declaration.ts
@@ -1,24 +1,58 @@
-function f( shouldInitialize: boolean ) {
-	if( shouldInitialize ) {
+function ScopingRules(shouldInitialize: boolean) {
+	if (shouldInitialize) {
 		var x = 10;
 	}
 	return x;
 }
 
-f(true); // returns '10'
-f(false); // returns 'undefined';
+ScopingRules(true); // returns '10'
+ScopingRules(false); // returns 'undefined';
+
+/**
+ * These scoping rules can cause several types of mistakes.
+ * One problem they exacerbate is the fact that it is not an error to declare the same variable multiple times:
+ * 
+ */
+
+function sumMatrix(matrix: number[][]) {
+	var sum = 0;
+	for (var i = 0; i < matrix.length; i++) {
+		var currentRow = matrix[i];
+		for (var i = 0; i < currentRow.length; i++) {
+			sum += currentRow[i];
+		}
+
+	}
+}
+
+/**
+ * 
+ */
+
 
 
 /***** Block-scoping ****/
-function letFunction(input: boolean){
+function letFunction(input: boolean) {
 	let a = 100;
-	
-	if(input){
+
+	if (input) {
 		// Still okay to reference 'a'
 		let b = a + 1;
 		return b;
-		
+
 	}
 	// Error: 'b' doesn't exist here 
 	return b;
 }
+
+
+for( var i = 0; i < 10; i++ ){
+	setTimeout(() => {
+		console.log(i);
+	}, 100 * i);
+}
+// for (let index = 0; index < 10; index++) {
+// 	setTimeout(() => {
+// 		console.log(i);
+// 	}, 100 * i);
+// }
---
+++ TypeScript/handbook/variable-declarations/main.js
@@ -0,0 +1,16 @@
+// for (var i = 0; i < 10; i++) {
+//     setTimeout(() => {
+//         console.log(i);
+//     }, 100 * i);   
+// }
+
+let arrayValue = ['a','b','c'];
+
+for (let index = 0; index < 10; index++) {
+
+    setTimeout(() => {
+        console.log(index);
+        
+    }, 100 * index);
+    
+}
