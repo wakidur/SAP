d55ebce835a9fef0352ab85fdf9409c6e4782343
wakidur <wakidr@gmail.com>
2018-06-03 19:49:27 +0600

    #add a

--- Angular.io/Components-&-Templates/Lifecycle-Hooks/picBooks/app/spy.directive.ts
+++ Angular.io/Components-&-Templates/Lifecycle-Hooks/picBooks/app/spy.directive.ts
@@ -4,23 +4,25 @@ import { LoggerService } from "./logger.servies";
 let nextId = 1;
 
 // Spy on any element to which it is applied. 
+// You can apply the spy to any native or component element and it'll be initialized and destroyed at the same time as that element. Here it is attached to the repeated hero <div>
 // Usage: <div mySpy> ... </div>
 
 @Directive({
     selector: '[mySpy]',
 })
 export class SpyDirective implements OnInit, OnDestroy { 
-    constructor(private logger: LoggerService) {}
+
+    constructor( private logger: LoggerService ) {}
 
     ngOnInit() {
-        //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-        //Add 'implements OnInit' to the class.
+        // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+        // Add 'implements OnInit' to the class.
         this.logIt(`OnInit`);
-
     }
+
     ngOnDestroy() {
-        //Called once, before the instance is destroyed.
-        //Add 'implements OnDestroy' to the class.
+        // Called once, before the instance is destroyed.
+        // Add 'implements OnDestroy' to the class.
         this.logIt('onDestroy');
     }
 
