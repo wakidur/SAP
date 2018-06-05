72a46fad8361f9fb8c193f413e3346f13349545f
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-06-05 10:53:35 +0600

    #works on componet intercation

---
+++ Angular.io/Components-&-Templates/component-interaction/component-interaction-project/app.e2e-spec.ts
@@ -0,0 +1,16 @@
+// ...
+let _heroNames = ['Mr. IQ', 'Magneta', 'Bombasto'];
+let _masterName = 'Master';
+
+it('should pass properties to children properly', function () {
+  let parent = element.all(by.tagName('app-hero-parent')).get(0);
+  let heroes = parent.all(by.tagName('app-hero-child'));
+
+  for (let i = 0; i < _heroNames.length; i++) {
+    let childTitle = heroes.get(i).element(by.tagName('h3')).getText();
+    let childDetail = heroes.get(i).element(by.tagName('p')).getText();
+    expect(childTitle).toEqual(_heroNames[i] + ' says:');
+    expect(childDetail).toContain(_masterName);
+  }
+});
+// ...
