f771baefa5ffde705dfa3bbe4b96905fd11d901f
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-07-10 17:36:36 +0600

    # add niw w

---
+++ helper/Getting-MEAN-with-MEAN-SecondEdition/5 Building a data model with MongoDB and Mongoose/db.js
@@ -0,0 +1,25 @@
+const dbURI = 'mongodb://localhost/Loc8r';
+mongoose.connect(dbURI);
+
+
+mongoose.connection.on('connected', () => {
+    console.log(`Mongoose connected to ${dbURI}`);
+});
+mongoose.connection.on('error', err => {
+    console.log('Mongoose connection error:', err);
+});
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected');
+});
+
+
+const readLine = require ('readline');
+if (process.platform === 'win32'){
+  const rl = readLine.createInterface ({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.on ('SIGINT', () => {
+    process.emit ("SIGINT");
+  });
+}
