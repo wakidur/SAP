3043ec17b2dd0dc4af692df20e21b3fdbe7354fb
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-07-23 15:38:46 +0600

    #start type script

---
+++ TypeScript/typeScriptCodePractice/basicType/basic-type.ts
@@ -0,0 +1,23 @@
+/**
+ * Boolean
+ */
+let isDone:  boolean = false;
+
+
+/**
+ * Number
+ */
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+
+/**
+ * String
+ */
+
+let color: string = "blue";
+
+let fullName: string = `Bob Bobbington`;
+let age: number = 36;
+let sentence: string = `Hello, my name is ${fullname }. i will be ${age + 1} years old next month.`;
