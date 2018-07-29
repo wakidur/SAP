115795d16cb264590576e575fb130dd7e8551112
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-07-29 13:49:03 +0600

    #add

--- TypeScript/typeScriptCodePractice/Interface/interface.js
+++ TypeScript/typeScriptCodePractice/Interface/interface.js
@@ -14,4 +14,29 @@ var labelObj = {
     label: "Size 10 Object"
 };
 printLabelWithInte(labelObj);
+function createSquare(config) {
+    var newSquare = { color: "white", area: 100 };
+    if (config.color) {
+        newSquare.color = config.color;
+    }
+    if (config.width) {
+        newSquare.area = config.width * config.width;
+    }
+    return newSquare;
+}
+var mySqueay = createSquare({ color: "black" });
+function createSquare1(config) {
+    var newSquare = {
+        color: "white",
+        area: 100
+    };
+    if (config.color) {
+        newSquare.color = config.color;
+    }
+    if (config.width) {
+        newSquare.area = config.width * config.width;
+    }
+    return newSquare;
+}
+var mySqueay1 = createSquare({ color: "black" });
 //# sourceMappingURL=interface.js.map
