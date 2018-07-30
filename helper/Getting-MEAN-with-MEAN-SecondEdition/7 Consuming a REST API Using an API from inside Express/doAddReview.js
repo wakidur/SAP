8c1f867e3195ebd3c788e843b73daef07487de32
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-07-30 14:15:43 +0600

    #add new file

--- helper/Getting-MEAN-with-MEAN-SecondEdition/7 Consuming a REST API Using an API from inside Express/doAddReview.js
+++ helper/Getting-MEAN-with-MEAN-SecondEdition/7 Consuming a REST API Using an API from inside Express/doAddReview.js
@@ -57,3 +57,36 @@ const doAddReview = function (req, res) {
         }
     })
 };
+
+/**
+ * Adding some simple validation to an Express controller
+ */
+
+const doAddReview_2 = function (req, res) {
+    const locationid = req.params.locationid;
+    const path = `/api/locations/${locationid}/reviews`;
+    const postdata = {
+        author: req.body.name,
+        rating: parseInt(req.body.rating, 10),
+        reviewText: req.body.review,
+    };
+    const requestOptions = {
+        url: apiOption.server + path,
+        method: "POST",
+        json: postdata,
+    };
+
+    if (!postdata.author || !posdata.rating || !posdata.reviewText) {
+        res.redirect(`/location/${locationid}/review/new?err=val`);
+    } else {
+        request(requestOptions, (err, response, body) => {
+            if (respons.statusCode === 201) {
+                res.redirect(`/locaton/${locationid}`);
+            } else if(response.statusCode === 400 && body.name && body.name === "ValidationError") {
+                res.redirect(`/location/${locationid}/review/new?err=val`);
+            } else {
+                _showError(req, res, response.statusCode);
+            }
+        });
+    }
+};
--- helper/Getting-MEAN-with-MEAN-SecondEdition/7 Consuming a REST API Using an API from inside Express/renderReviewForm.js
+++ helper/Getting-MEAN-with-MEAN-SecondEdition/7 Consuming a REST API Using an API from inside Express/renderReviewForm.js
@@ -16,3 +16,14 @@ const posdata = {
     rating: parseInt(req.body.rating, 10),
     reviewText: req.body.review
 };
+
+/**
+ * Update the controller to pass a query string error string to the view
+ */
+const _renderReviewForm = function (req, res, locDetail) {
+    res.render('location-review-form', {
+        title: `Review ${locDetail.name} on Loc8r`,
+        pageHeader: {title: `Review ${locDetail.name}`},
+        error: req.query.err,
+    });
+};
