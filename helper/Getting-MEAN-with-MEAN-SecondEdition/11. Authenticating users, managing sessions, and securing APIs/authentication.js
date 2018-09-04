6871f2de0ce891e6c8be4dfa5f90588365566fd5
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-09-04 11:32:34 +0600

    #new file add

--- helper/Getting-MEAN-with-MEAN-SecondEdition/11. Authenticating users, managing sessions, and securing APIs/authentication.js
+++ helper/Getting-MEAN-with-MEAN-SecondEdition/11. Authenticating users, managing sessions, and securing APIs/authentication.js
@@ -7,12 +7,17 @@ const sendJSONresponse = function (res, status, content) {
     res.json(content);
 }
 
-module.exports.register = function (req, res) {
+/**
+ * Register controller for the API
+ */
+function register(req, res) {
 
     // Respond with an error status if not all required fields are found
     if (!req.body.name || !req.body.email || !req.body.password) {
-        sendJSONresponse(req, 400, {"message": "All fields required"});
-        return
+        sendJSONresponse(req, 400, { 
+            "message": "All fields required" 
+        });
+        return;
     }
 
     // Create a new user instance and set name and email
@@ -20,17 +25,57 @@ module.exports.register = function (req, res) {
     
     user.name = req.body.name;
     user.email = req.body.email;
-    user.setPassword(req.body.passport); // use setPassword method to set salt and hash
+    // use setPassword method to set salt and hash
+    user.setPassword(req.body.passport); 
 
-    user.save(function (err) {
-        // save new user to MongoDB
-        const token;
+    // save new user to MongoDB
+    user.save( (err) => {
+        
+        let token;
         if (err) {
             sendJSONresponse(res, 404, err);
-        } else{
+        } else {
             // Generate a JWT using schema method and send it to browser
             token = user.generateJwt();
-            sendJSONresponse(res, 200, {"token": token});
+            sendJSONresponse(res, 200, {
+                "token": token
+            });
         }
     });  
+}
+
+
+function login(req, res) {
+    // validate that required fields have been supplied
+    if ( !req.body.email || !req.body.password) {
+        sendJSONresponse(res, 400, {
+            "message": "All fields required"
+        });
+        return;
+    }
+    // Pass name of strategy and a callback to authonticate method
+    passport.authenticate('local', (err, user, info) => {
+        let token;
+        if (err) {
+            // return an error if passport returns an error
+            sendJSONresponse(res, 404, err);
+        }
+        if (user) {
+            // if passport returned a user instance, generate and send a JWT
+            token = user.generateJwt();
+            sendJSONresponse(res, 200, {
+                "token" : token
+            });
+        } else {
+            // otherwise return info message(why authoenicate failed)
+            sendJSONresponse(res, 401, info);
+        }
+    })(req, res);// make sure that req and res are available to passport
+}
+
+
+
+module.exports = {
+    register,
+    login
 }
