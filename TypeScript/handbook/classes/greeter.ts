8c54339e388c46fe217761197743f75de5b87e71
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-10-14 09:50:11 +0600

    # ss 14-10-18 class inheritance

--- TypeScript/handbook/classes/greeter.ts
+++ TypeScript/handbook/classes/greeter.ts
@@ -30,3 +30,46 @@ const dogs = new Dog();
 dog.bark();
 dog.move();
 dog.bark();
+
+/**
+ * More complex example
+ */
+class AnimalSecond {
+    name: string;
+    constructor(theName: string) {
+        this.name = theName;
+    }
+    move(distanceInMeters: number = 0) {
+        console.log(`${this.name} moved ${distanceInMeters}m.`);
+    }
+}
+
+class Snaks extends AnimalSecond {
+    constructor(name: string) {
+        super(name); 
+    }
+    move(distanceInMeters = 5) {
+        console.log("Slithering...");
+        super.move(distanceInMeters);
+    }
+}
+
+class Horses extends AnimalSecond {
+    constructor(name: string) {
+        super(name);
+    }
+    move(distanceInMeters = 45) {
+        console.log("Galloping....");
+        super.move(distanceInMeters);
+    }
+}
+
+let sams = new Snaks("Sammy the Python");
+let toms: AnimalSecond = new Horses('Tommy the Palomino');
+
+sam.move();
+toms.move(35);
+
+/**
+ * Public, private, and protected modifiers
+ */
