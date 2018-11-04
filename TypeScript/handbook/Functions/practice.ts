f4c859d8ad21e718d1ada93d87197a10a83524f4
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-11-04 08:59:56 +0600

    #ff sss

--- TypeScript/handbook/Functions/practice.ts
+++ TypeScript/handbook/Functions/practice.ts
@@ -100,6 +100,8 @@ let buildNameFun: (fname: string, ...restName: string[]) => string = restParamet
 /**
  * this
  * this and arrow function
+ * this parameters
+ * this parameters in callbacks
  */
 
 let deck = {
@@ -149,23 +151,116 @@ let pickedCardArrow = cardPickerArrow();
 console.log(`card: ${pickedCardArrow.card} of ${pickedCardArrow.suit}`)
 alert(`card: ${pickedCardArrow.card} of ${pickedCardArrow.suit}`);
 
-var arr  = [1,2,3,4,5,6]
-var n = 3;
-function getRandom(arr, n) {
-  var result = new Array(n),
-      len = arr.length,
-      taken = new Array(len);
-  if (n > len)
-      throw new RangeError("getRandom: more elements taken than available");
-  var y = "";
-  while (n--) {
-      var x = Math.floor(Math.random() * len);
-      result[n] = arr[x in taken ? taken[x] : x];
-      taken[x] = --len in taken ? taken[len] : len;
-      debugger;
-      y += arr[x].toString();
-  }
-  return y;
+// this parameters
+
+function f(this: void) {
+  // make sure 'this' is unuable in this standalone function
 }
 
-getRandom(arr, n);
+interface CardInterface {
+  suit: string;
+  card: number;
+}
+
+interface DeckInterface {
+  suits: string[];
+  cards: number[];
+  createCardPicker(this: DeckInterface): () => CardInterface;
+}
+
+let deckInterfaceObject: DeckInterface = {
+  suits: ["hearts", "spades", "clubs", "diamonds"],
+  // Note: The function now explicitly speciafied that its callee must be of type DeckInterface
+  createCardPicker: function(this: DeckInterface) {
+    return () => {
+      let pickedCard = Math.floor(Math.random() * 52);
+      let pickedSuit = Math.floor(pickedCard / 13);
+      return {
+        suit: this.suits[pickedSuit],
+        card: pickedCard % 13
+      }
+    }
+  }
+}
+
+let cardPickerInterface = deck.createCardPicker();
+let pickedCardInterface = cardPickerInterface();
+
+console.log(`card: ${pickedCardInterface.card} of ${pickedCardInterface.suit}`);
+
+// this parameters in callbacks
+
+interface UIElement {
+  addClickListener(onclick: (this: void, e: Event) => void): void;
+}
+
+class Handler {
+  info: string;
+  conClickBad( this: Handler, e: Event) {
+    // oops, used this here . using this callback would crash at runtime
+    this.info = e.message;
+  }
+}
+
+let h = new Handler();
+
+/**
+ * Overloads
+ */
+
+
+let suits = ['hearts','spades', 'clubs', 'diamonds'];
+
+function pickCard(x): any {
+  // check to see if we're working with an object/array
+  // if so, they gave us the deck  and we'll pick the card
+  if (typeof x == "object") {
+    let pickedCard = Math.floor(Math.random() * x.length);
+    return pickedCard;
+  } 
+  // Otherwise jist let them pick the card
+  else if(typeof x ==  "number") {
+    let pickedSuit = Math.floor(x / 13);
+    return {
+      suit: suits[pickedSuit],
+      card: x % 13
+    }
+  }
+}
+
+let myDeckOver = [{
+  suit: "diamonds",
+  card: 2
+}, {
+  suit: "spades",
+  card: 10
+}, {
+  suit: "hearts",
+  card: 4
+}];
+let pickedCard1 = myDeckOver[pickCard(myDeckOver)];
+
+let suitss = ["hearts", "spades", "clubs", "diamonds"];
+
+function pickCards(x: {suit: string; card: number; }[]): number;
+function pickCards(x: number): {suit: string; card: number; };
+function pickCards(x): any {
+    // Check to see if we're working with an object/array
+    // if so, they gave us the deck and we'll pick the card
+    if (typeof x == "object") {
+        let pickedCard = Math.floor(Math.random() * x.length);
+        return pickedCard;
+    }
+    // Otherwise just let them pick the card
+    else if (typeof x == "number") {
+        let pickedSuit = Math.floor(x / 13);
+        return { suit: suitss[pickedSuit], card: x % 13 };
+    }
+}
+
+let myDeck = [{ suit: "diamonds", card: 2 }, { suit: "spades", card: 10 }, { suit: "hearts", card: 4 }];
+let pickedCard2 = myDeck[pickCard(myDeck)];
+alert("card: " + pickedCard1.card + " of " + pickedCard1.suit);
+
+let pickedCard2 = pickCard(15);
+alert("card: " + pickedCard2.card + " of " + pickedCard2.suit);
