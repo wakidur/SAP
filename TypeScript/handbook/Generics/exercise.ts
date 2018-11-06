79ba7358416aec420b9ea0e5e28e307756a25c1e
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-11-06 10:00:07 +0600

    #nice jj

--- TypeScript/handbook/Generics/exercise.ts
+++ TypeScript/handbook/Generics/exercise.ts
@@ -1,11 +1,53 @@
-function identity(arg:type): number {
+function identity(arg: number): number {
+  return arg;
+}
+
+function identityAny(arg: any): any {
+  return arg;
+}
+
+function identityType<T>(arg: T): T {
+  return arg;
+}
+
+/**
+ * 06-11-2018 -----------------
+ */
+
+function identitySpecificType(arg: number): number {
     return arg;
 }
 
-function identityAny(arg:type): any {
+function identityAnyType(arg: any): any {
     return arg;
 }
 
-function identityType<T> (arg: T): T {
+
+function identityTypeVariable<T>(arg: T): T {
+    return arg;
+}
+
+// Once we’ve written the generic identity function, we can call it in one of two ways. 
+let outputFirst = identityTypeVariable<string>('myString'); // type of output will be 'string'
+
+// The second way is also perhaps the most common. Here we use type argument inference – that is, we want the compiler to set the value of T for us automatically based on the type of the argument we pass in
+let outputSecond = identityTypeVariable('myString'); // type of output will be 'string'
+
+function identityTypeOne<T>(arg: T): T {
+    return arg;
+}
+
+function loggingIdentity<T>(arg: T): T {
+    console.log(arg.length);// Error: T doesn't have .length
+    return arg;
+}
+
+function loggingIdentitysecond<T>(arg: T[]): T[] {
+    console.log(arg.length);// Error: T doesn't have .length
+    return arg;
+}
+
+function loggingIdentityThird<T>(arg: Array<T>): Array<T> {
+    console.log(arg.length);// Error: T doesn't have .length
     return arg;
 }
