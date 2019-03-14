42419b03e16500b94eff6d5a4f92deea4792f01e
NEXT_IT\Wakidur <wakidur@gmail.com>
2019-03-14 10:22:51 +0600

    #dfdfdfaaa

--- Angular.io/Books-Study/Architecting Angular Applications with Redux, RxJS, and NgRx- 01-03-2019/3. Asynchronous Programming/example.js
+++ Angular.io/Books-Study/Architecting Angular Applications with Redux, RxJS, and NgRx- 01-03-2019/3. Asynchronous Programming/example.js
@@ -43,7 +43,94 @@ function getMoreDataCallback(err, moreData) {
 }
 
 function getDataCallback(err, data) {
-    getMoreData('url'+data.id, getMoreDataCallback);
-    
+    getMoreData('url' + data.id, getMoreDataCallback);
+
 }
 getData('url', getDataCallback);
+
+
+// Promis
+/**
+ * To put a promis to use, we need to instantiate it and make it part of a method 
+ */
+
+function getData(params) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('data');
+        }, 1000);
+    });
+}
+
+function getMoreData() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject('error from more data')
+        }, 1000);
+    })
+}
+
+// then
+getData().then((data) => {
+    console.log('data', data);
+});
+
+// alternative 1
+getMoreData().then(
+    data => {
+        console.log('data', data);
+    },
+    err => {
+        console.log('error', err);
+    }
+);
+
+// alternative 2
+getMoreData().then(data => {
+        console.log('data', data);
+    })
+    .catch((err) => {
+        console.log('error', err);
+    });
+
+// Async/await
+async function getData(arguments) {
+    // more to come
+}
+
+async function getData(arguments) {
+    let data = await getMoreData();
+    let otherData = await getEvenMoreData();
+}
+
+// promise-based 
+function getMoreData(params) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('more data')
+        }, 100);
+    });
+}
+
+function getEvenMoreData(params) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('more data');
+        }, 1000);
+    });
+}
+
+/**
+ * When we call getMoreData() and getEvenMoreData(), 
+ * we can now use the keyword await to wait for the promise to resolve
+ */
+
+async function getData(arguments) {
+    var data = await Promise.resolve('data');
+    var moreData = await getMoreData(data);
+    var evenMoreData = await getEvenMoreData(moreData);
+    return evenMoreData;
+
+}
+
+getData().then((result) => console.log('result', result) );
