10bc23cb3975baff1864343214c4911ec0553b25
NEXT_IT\Wakidur <wakidur@gmail.com>
2019-03-19 19:34:43 +0600

    #fdfdfaaa

--- Angular.io/Books-Study/Architecting Angular Applications with Redux, RxJS, and NgRx- 01-03-2019/3. Asynchronous Programming/understanding-asynchronous-javascript.js
+++ Angular.io/Books-Study/Architecting Angular Applications with Redux, RxJS, and NgRx- 01-03-2019/3. Asynchronous Programming/understanding-asynchronous-javascript.js
@@ -1,3 +1,4 @@
+// Synchronous - 1
 const second = ()=> {
     console.log(`2. Second Function: Hello there!`);
 }
@@ -8,4 +9,51 @@ const first = () => {
     console.log('1.2 First Function: The End');
 };
 
-first()
+first();
+
+// Synchronous - 2
+
+const processImage = (image) => {
+    /**
+     * Doing some operations on image
+     */
+    console.log("Image Processed");
+};
+
+const networkRequest = (url) => {
+    /**
+     * requesting network resource
+     */
+    return someData;
+};
+
+const greeting = () => {
+    console.log('Hello World');
+};
+
+processImage(logo.jpg);
+networkRequest('www.somerandomurl.com');
+greeting();
+
+
+/**
+ * asynchronous 
+ */
+// asynchronous  Callback
+const networkRequestAsync = () => {
+    setTimeout(() => {
+        console.log('Async code');
+    }, 2000);
+};
+
+console.log('Hello world');
+
+// how it’s executed in an asynchronous way.
+const networkRequestAsyncProcess = () => {
+    setTimeout(() => {
+      console.log('Async Code');
+    }, 2000);
+  };
+  console.log('Hello World');
+  networkRequestAsyncProcess();
+  console.log('The End');
