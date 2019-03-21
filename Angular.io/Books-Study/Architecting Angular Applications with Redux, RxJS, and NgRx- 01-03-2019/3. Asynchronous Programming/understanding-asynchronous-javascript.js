2ba96f10bd7773e50abe1d53c34d68597157cda3
NEXT_IT\Wakidur <wakidur@gmail.com>
2019-03-21 11:26:37 +0600

    #aaass ss

--- Angular.io/Books-Study/Architecting Angular Applications with Redux, RxJS, and NgRx- 01-03-2019/3. Asynchronous Programming/understanding-asynchronous-javascript.js
+++ Angular.io/Books-Study/Architecting Angular Applications with Redux, RxJS, and NgRx- 01-03-2019/3. Asynchronous Programming/understanding-asynchronous-javascript.js
@@ -1,5 +1,5 @@
 // Synchronous - 1
-const second = ()=> {
+const second = () => {
     console.log(`2. Second Function: Hello there!`);
 }
 
@@ -51,15 +51,90 @@ console.log('Hello world');
 // how it’s executed in an asynchronous way.
 const networkRequestAsyncProcess = () => {
     setTimeout(() => {
-      console.log('Async Code');
+        console.log('Async Code');
     }, 2000);
-  };
-  console.log('Hello World');
-  networkRequestAsyncProcess();
-  console.log('The End');
+};
+console.log('Hello World');
+networkRequestAsyncProcess();
+console.log('The End');
 
 
 // DOM Events
 document.querySelector('.btn').addEventListener('click', (event) => {
     console.log("Button Clicked");
 })
+
+// ES6 Job Queue/ Micro-Task queue
+
+// step - 1
+console.log('Script start');
+
+setTimeout(() => {
+    console.log('setTimeout');
+}, 0);
+
+new Promise((resolve, reject) => {
+    resolve('Promise resolved');
+}).then((result) => {
+    console.log(result);
+}).catch((err) => {
+    console.error(err);
+});
+
+console.log('Script End');
+
+// step - 2
+console.log('Script start');
+
+setTimeout(() => {
+    console.log('setTimeout 1');
+}, 0);
+
+setTimeout(() => {
+    console.log('setTimeout  2 ');
+}, 0);
+
+new Promise((resolve, reject) => {
+    resolve('Promise 1 resolve');
+}).then((result) => {
+    console.log(result);
+}).catch((err) => {
+    console.log(err);
+});
+new Promise((resolve, reject) => {
+    resolve('Promise 2 resolved');
+}).then((result) => {
+    console.log(result);
+}).catch((err) => {
+    console.log(err);
+});
+console.log('Script End');
+
+// step - 3
+console.log('Script start');
+setTimeout(() => {
+    console.log('setTimeout');
+}, 0);
+
+new Promise((resolve, reject) => {
+    resolve('Promise 1 resolved');
+}).then((result) => {
+    console.log(result);
+}).catch((err) => {
+    console.log(err);
+});
+
+new Promise((resolve, reject) => {
+    resolve('Promise 2 resolve');
+}).then((result) => {
+    console.log(result);
+    return new Promise((resolve, reject) => {
+        resolve('Promise 3 resolved');
+    }).then((result) => {
+        console.log('result');
+    }).catch((err) => {
+        console.log(err);
+    });
+}).catch((err) => {
+    console.log(err);
+});
