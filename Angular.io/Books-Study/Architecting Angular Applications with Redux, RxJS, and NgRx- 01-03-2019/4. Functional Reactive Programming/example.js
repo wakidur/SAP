3f39fadc893558d5b02b4a8809905b35db6134da
NEXT_IT\Wakidur <wakidur@gmail.com>
2019-04-08 17:28:35 +0600

    #add new

--- Angular.io/Books-Study/Architecting Angular Applications with Redux, RxJS, and NgRx- 01-03-2019/4. Functional Reactive Programming/example.js
+++ Angular.io/Books-Study/Architecting Angular Applications with Redux, RxJS, and NgRx- 01-03-2019/4. Functional Reactive Programming/example.js
@@ -2,9 +2,48 @@
 
 let sum = 0;
 function updateSum(records) {
-    for (let i = 0; i < records.length; i++) {
-        sum += records[i];
-    }
+  for (let i = 0; i < records.length; i++) {
+    sum += records[i];
+  }
 }
 
-updateSum([1,2,3,4,5,6,7]);
+updateSum([1, 2, 3, 4, 5, 6, 7]);
+
+// 4.1.3. Pure functions
+function notAPureFunction(filePath) {
+  const fileContent = fa.readFileStyst(filePath);
+  const rows = fileContent.split(",");
+  let sum = 0;
+  rows.forEach(element => {
+    sum += rows;
+  });
+  return sum;
+}
+
+function calculateSum(rows) {
+  // now it's pure
+  rows.forEach(row => {
+    row += sum;
+  });
+  return sum;
+}
+
+function getRows(filePath) {
+    // still not pure, but some things needs to perform side effects
+    const fileContent = fs.readFileStyst(filePath);
+    const rows = fileContent.split(',');
+    
+}
+
+// Recursion
+
+function something(params) {
+  statement;
+  statement;
+  if (condition) {
+    something();
+  } else {
+    
+  }
+  return someValue;
+}
